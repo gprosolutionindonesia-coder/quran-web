9d23de9a93db8d5485eac87ea6cccd4615551402
agussupriono0302 <clevguard.id@gmail.com>
2026-06-27 22:13:49 +0700

    Perbaiki orientasi mushaf dan navigasi RTL

--- js/app.js
+++ js/app.js
@@ -1,51 +1,70 @@
+// Quran Web GPRO
+// Viewer RTL Mushaf
 
 let page = 2;
 
+// Elemen
 const leftPage = document.getElementById("leftPage");
 const rightPage = document.getElementById("rightPage");
 const pageNumber = document.getElementById("pageNumber");
 
-function updatePages(){
+// Konversi angka Latin ke angka Arab
+function toArabicNumber(num) {
 
-    // Halaman GANJIL di kiri
-    leftPage.src =
-    "Medina1/data/N/" +
-    String(page + 1).padStart(4,"0") +
-    ".gif";
+    const arabic = ["٠","١","٢","٣","٤","٥","٦","٧","٨","٩"];
 
-    // Halaman GENAP di kanan
-    rightPage.src =
-    "Medina1/data/N/" +
-    String(page).padStart(4,"0") +
-    ".gif";
-
-    pageNumber.innerHTML =
-    "Halaman " + page + " - " + (page + 1);
+    return num.toString().replace(/\d/g, d => arabic[d]);
 
 }
 
+// Update tampilan
+function updatePages() {
+
+    // Halaman kiri = ganjil
+    leftPage.src =
+        "Medina1/data/N/" +
+        String(page + 1).padStart(4, "0") +
+        ".gif";
+
+    // Halaman kanan = genap
+    rightPage.src =
+        "Medina1/data/N/" +
+        String(page).padStart(4, "0") +
+        ".gif";
+
+    // Nomor halaman
+    pageNumber.innerHTML =
+        toArabicNumber(page) +
+        " | " +
+        toArabicNumber(page + 1);
+
+}
+
+// Tampilkan pertama kali
 updatePages();
 
-document.getElementById("next").onclick=function(){
+// Tombol Berikutnya
+document.getElementById("next").onclick = function () {
 
-    if(page<622){
+    if (page < 622) {
 
-        page+=2;
-
-        updatePages();
-
-    }
-
-}
-
-document.getElementById("prev").onclick=function(){
-
-    if(page>2){
-
-        page-=2;
+        page += 2;
 
         updatePages();
 
     }
 
-}
+};
+
+// Tombol Sebelumnya
+document.getElementById("prev").onclick = function () {
+
+    if (page > 2) {
+
+        page -= 2;
+
+        updatePages();
+
+    }
+
+};
