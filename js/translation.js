85c3d51ba689b6a89c49bb9c5ad1c546ad2d381c
agussupriono0302 <clevguard.id@gmail.com>
2026-06-30 18:58:29 +0700

    Update translation.js

--- js/translation.js
+++ js/translation.js
@@ -13,6 +13,17 @@ window.loadTranslation = async function (surahNumber) {
 
     currentSurah = surahNumber;
 
+    console.log("==============================");
+    console.log("loadTranslation()");
+    console.log("Surah Number :", surahNumber);
+
+    const file =
+        "data/translation/id/id_translation_" +
+        surahNumber +
+        ".json";
+
+    console.log("File :", file);
+
     const panel = document.getElementById("translationPanel");
 
     panel.innerHTML = `
@@ -23,25 +34,30 @@ window.loadTranslation = async function (surahNumber) {
 
     try {
 
-        const response = await fetch(
-            "data/translation/id/id_translation_" +
-            surahNumber +
-            ".json"
-        );
+        const response = await fetch(file);
+
+        console.log("Status :", response.status);
+
+        if (!response.ok) {
+            throw new Error("File tidak ditemukan");
+        }
 
         const data = await response.json();
+
+        console.log("Nama Surat :", data.name);
 
         renderTranslation(data);
 
     } catch (err) {
 
+        console.error(err);
+
         panel.innerHTML = `
             <div class="alert alert-danger">
-                Gagal memuat terjemahan.
+                Gagal memuat terjemahan<br>
+                <small>${file}</small>
             </div>
         `;
-
-        console.error(err);
 
     }
 
@@ -70,31 +86,21 @@ function renderTranslation(data) {
     for (let i = 1; i <= data.count; i++) {
 
         html += `
-
         <div class="card mb-3 shadow-sm">
 
             <div class="card-body">
 
-                <span
-                    class="badge bg-success mb-2">
-
+                <span class="badge bg-success mb-2">
                     Ayat ${i}
-
                 </span>
 
-                <div
-                    style="
-                    line-height:1.8;
-                    font-size:16px;">
-
+                <div style="line-height:1.8;font-size:16px;">
                     ${data.verse["verse_" + i]}
-
                 </div>
 
             </div>
 
         </div>
-
         `;
 
     }
@@ -102,10 +108,6 @@ function renderTranslation(data) {
     panel.innerHTML = html;
 
 }
-
-// ======================================
-// Default
-// ======================================
 
 loadTranslation(1);
 
